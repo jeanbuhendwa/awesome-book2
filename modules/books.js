52a604c90cd63fd83f8e1f7ef11efe5ac8a0d774
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-02-20 16:01:59 +0300

    Update book.js to arrow function

--- modules/books.js
+++ modules/books.js
@@ -1,19 +1,19 @@
-const bookList = document.querySelector('.books');
-const bookName = document.getElementById('book_name');
-const autName = document.getElementById('auther_name');
+const bookList = document.querySelector(".books");
+const bookName = document.getElementById("book_name");
+const autName = document.getElementById("auther_name");
 
 export default class Book {
   constructor() {
     this.bookStore = [
       {
-        id: 'book_0',
-        title: 'object-oriented analysis and design',
-        author: 'Martin B',
+        id: "book_0",
+        title: "object-oriented analysis and design",
+        author: "Martin B",
       },
       {
-        id: 'book_1',
-        title: 'software design principles',
-        author: 'Jose K',
+        id: "book_1",
+        title: "software design principles",
+        author: "Jose K",
       },
     ];
     this.bookCount = this.bookStore.length - 1;
@@ -31,7 +31,7 @@ export default class Book {
     };
   }
 
-  addBook() {
+  addBook = () => {
     this.bookCount += 1;
     const objBook = {
       id: `book_${this.bookCount}`,
@@ -43,27 +43,27 @@ export default class Book {
     this.displayBook(objBook);
     this.updateLocalStorage();
 
-    bookName.value = '';
-    autName.value = '';
-  }
+    bookName.value = "";
+    autName.value = "";
+  };
 
-  removeBook(e) {
+  removeBook = (e) => {
     const parentli = e.target.parentElement;
     this.bookStore = this.bookStore.filter((book) => book.id !== parentli.id);
     bookList.removeChild(e.target.parentElement);
     this.updateLocalStorage();
-  }
+  };
 
-  restoreLocalStorage() {
-    this.bookStore = JSON.parse(localStorage.getItem('book_list'));
-    this.bookCount = JSON.parse(localStorage.getItem('book_count'));
+  restoreLocalStorage = () => {
+    this.bookStore = JSON.parse(localStorage.getItem("book_list"));
+    this.bookCount = JSON.parse(localStorage.getItem("book_count"));
     for (let i = 0; i < this.bookStore.length; i += 1) {
       this.displayBook(this.bookStore[i]);
     }
-  }
+  };
 
-  updateLocalStorage() {
-    localStorage.setItem('book_list', JSON.stringify(this.bookStore));
-    localStorage.setItem('book_count', JSON.stringify(this.bookCount));
-  }
+  updateLocalStorage = () => {
+    localStorage.setItem("book_list", JSON.stringify(this.bookStore));
+    localStorage.setItem("book_count", JSON.stringify(this.bookCount));
+  };
 }
